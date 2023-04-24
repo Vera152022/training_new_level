ebccd09ba9398474e4dc7fff5da89b621b5ef4ab
Vera152022 <118746256+Vera152022@users.noreply.github.com>
2023-04-24 09:48:37 +0300

    вроде норм

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from data import db_session
 from data.users import User
 from forms.user import RegisterForm
 from forms.login import LoginForm
-from flask_login import LoginManager, login_user, login_required, current_user
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 
 
 signup_is_on = True
@@ -19,8 +19,6 @@ login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/media/from_users'
 app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
-
-
 
 
 @login_manager.user_loader
@@ -47,7 +45,14 @@ def login():
 @app.route("/", methods=["GET", "POST"])
 def choice():
     if request.method == "GET":
-        return render_template("major.html")
+        if current_user.is_authenticated:
+            user = db_sess.query(User).filter(User.id == current_user.id).first()
+            name = user.name
+            db_sess.commit()
+        else:
+
+            name = 'Неизвестный гость'
+        return render_template("major.html", name=name)
     elif request.method == "POST":
         if request.form["choice"] == "one":
             return redirect("/lesson/1")
@@ -64,7 +69,6 @@ def choice():
         print(request.form["choice"])
 
 
-flag = 1
 tot = []
 good = 0
 
@@ -72,7 +76,7 @@ good = 0
 @login_required
 @app.route("/lesson/<int:site_id>", methods=["GET", "POST"])
 def index1(site_id):
-    global flag, tot, good
+    global tot, good
     good = 0
     if request.method == "GET":
         if site_id == 1:
@@ -94,10 +98,10 @@ def index1(site_id):
             if str(request.form[cells[i]]) == str(tot[1][i]):
                 good += 1
         print(good)
-        flag = 1
         return redirect(f"/result")
 
 
+@login_required
 @app.route("/result", methods=["GET"])
 def result():
     global good
@@ -119,14 +123,23 @@ def result():
         return render_template("result.html", name=names[number], text=word[number2])
 
 
+@login_required
+@app.route("/statistics", methods=["GET"])
+def statistic():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == current_user.id).first()
+    all = user.all_test
+    good = user.true_test
+    db_sess.commit()
+    return render_template("statistics.html", all=all, good=good)
+
+
 def add_result(good):
-    print(3)
     good = 20
     db_sess = db_session.create_session()
     user = db_sess.query(User).filter(User.id == current_user.id).first()
     user.all_test = str(int(user.all_test) + 1)
     if good == 20:
-        print(4)
         user.true_test = str(int(user.true_test) + 1)
     db_sess.commit()
 
@@ -147,11 +160,7 @@ def plus_minus():
             else:
                 dictionary[i] = number - number_2
                 example.append(f'{number} - {number_2} = ')
-    print(example)
-    print(dictionary)
-    print(11)
     add(example, dictionary)
-
     return [example, dictionary]
 
 
@@ -181,8 +190,7 @@ def multiplication_division():
                 number_2 = number_2 + (number - (number_2 % number))
                 dictionary[i] = number_2 // number
                 example.append(f'{number_2} ÷ {number} = ')
-    print(example)
-    print(dictionary)
+    add(example, dictionary)
     return [example, dictionary]
 
 
@@ -199,6 +207,7 @@ def plus_minus_multiplication():
         if i % 2 == 1:
             dictionary[i] = number_2 * number_3 - number
             example.append(f'{number_2} * {number_3} - {number} = ')
+    add(example, dictionary)
     return [example, dictionary]
 
 
@@ -233,6 +242,7 @@ def plus_minus_division():
                 else:
                     dictionary[i] = number_3 - number // number_2
                     example.append(f'{number_3} - {number} ÷ {number_2} = ')
+    add(example, dictionary)
     return [example, dictionary]
 
 
@@ -259,6 +269,7 @@ def multiplication_division_2():
                         number_3 += 1
                 dictionary[i] = number * number_2 // number_3
                 example.append(f'{number} * {number_2} ÷ {number_3} = ')
+    add(example, dictionary)
     return [example, dictionary]
 
 
@@ -270,15 +281,13 @@ def multiplication():
         number_2 = random.randint(2, 10)
         dictionary[i] = number_2 * number
         example.append(f'{number} * {number_2} = ')
-    print(example)
-    print(dictionary)
+    add(example, dictionary)
     return [example, dictionary]
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
-
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
             return render_template('register.html', title='Регистрация',
@@ -304,10 +313,26 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
+@login_required
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.errorhandler(404)
+def e404(code):
+    print(code)
+    return render_template("error.html", err=", этой страницы не существует :)")
+
+
+@app.errorhandler(500)
+def e500(code):
+    print(code)
+    return render_template("error.html", err=", Вам необходимо зарегистрироваться")
+
 
 if __name__ == "__main__":
-    # db = DB("./db", "template.db")
-    # db.global_init()
     db_session.global_init("db/user.db")
     db_sess = db_session.create_session()
     app.run(host="0.0.0.0", port=8080)
